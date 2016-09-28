3ea8b8e69e5cf294751748a2fb413722aa6acefd
Tyler Haas <Tyler.Haas@maritz.com>
2016-09-28 18:04:52 -0500

    addRecipe method

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var Main = React.createClass({
-  render() {
-    return (
-      <div>
-        Hello Jimmy
-      </div>
-    );
+class RecipeBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      recipeName: '',
+      recipeIngredientList: '',
+      recipeInstructions: '',
+      recipes: []
+    }
   }
-});
+
+  // pre-bind fn's
+  this.toggleModal = this.toggleModal.bind(this);
+  this.addRecipe = this.addRecipe.bind(this);
+  this.recipeNameChange = this.recipeNameChange.bind(this);
+  this.recipeIngredientListChange = this.recipeIngredientListChange.bind(this);
+  this.recipeInstructionsChange = this.recipeInstructionsChange.bind(this);
+
+  // class methods
+  addRecipe() {
+    const newRecipe = [
+      {
+        title: this.state.recipeName,
+        ingredients: this.state.recipeIngredientList.split(','),
+        instructions: this.state.recipeInstructions
+      }
+    ];
+
+    const newRecipeList = this.state.recipes.concat(newRecipe);
+
+    this.setState({recipes: newRecipeList, recipeName: '', recipeIngredientList: '', recipeInstructions: ''});
+
+    console.log({newRecipeList});
+
+    this.toggleModal();
+  }
+
+  toggleModal() {
+    this.setState({showModal: !this.state.showModal});
+  }
+
+}
 
 ReactDOM.render(<Main />, document.getElementById('app'));
