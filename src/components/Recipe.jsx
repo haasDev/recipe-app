54990a861a2783700547dc31e9b01e7390c91ed5
haas <haasdevelopment@gmail.com>
2016-10-05 23:23:22 -0500

    Recipe Component

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,5 +1,24 @@
+import Button from 'react-bootstrap/lib/Button';
+import Glyphicon from 'react-bootstrap/lib/Button';
+import ListGroup from 'react-bootstrap/lib/ListGroup';
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
+import Panel from 'react-bootstrap/lib/PanelGroup';
 
 const Recipe = ({title, ingredients, instructions}) => {
   const ingredientList = ingredients.map(ingredient => <ListGroupItem><Ingredient name={ingredient} /></ListGroupItem>);
+  return (
+    <Panel collapsible header={title}>
+      <ListGroup fill>
+        {ingredientList}
+      </ListGroup>
+      <div>
+        <p>Instructions:</p>
+        <p>{instructions}</p>
+      </div>
+      <div className="button-group">
+        <Button bsStyle="danger"><Glyphicon glyph="remove" /> Remove</Button>
+        <Button bsStyle="warning"><Glyphicon glyph="pencil" /> Edit</Button>
+      </div>
+    </Panel>
+  );
 };
