3858060f8cebd86239e3881e00812b88dc9b6a38
haas <haasdevelopment@gmail.com>
2016-10-05 21:05:21 -0500

    import ListGroupItem

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,3 +1,5 @@
+import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
+
 const Recipe = ({title, ingredients, instructions}) => {
   const ingredientList = ingredients.map(ingredient => <ListGroupItem><Ingredient name={ingredient} /></ListGroupItem>);
 };
